6bc39c7c53782d31985e0b0a67f59d3a24100969
Stanislav Kholodar <kholodar.stas@gmail.com>
2026-04-26 01:50:20 +0300

    Remove unused Prism preview code

--- src/lib/highlight.tsx
+++
@@ -1,8 +0,0 @@
-import Prism from 'prismjs/components/prism-core';
-import 'prismjs/components/prism-clike';
-import 'prismjs/components/prism-javascript';
-import 'prismjs/components/prism-typescript';
-
-export function highlightTypeScript(source: string): string {
-  return Prism.highlight(source, Prism.languages.typescript, 'typescript');
-}
